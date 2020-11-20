c261fca23aeebdf962bcb2b636be0ae64b9adf00
parinvasudevan <68951819+parinvasudevan@users.noreply.github.com>
2020-11-20 15:56:01 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,26 @@
+class Paper {
+    constructor (x, y, radius){
+        var options = {
+            isStatic : false,
+            restitution : 0.3,
+            friction : 0.5,
+            density : 1.2
+        }
+    this.body=Bodies.circle(x,y,radius,options)
+    this.r=radius
+      }
+display() {
+    
+    var paperpos=this.body.position;		
+
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			strokeWeight(3);
+			fill(255,0,255)
+			ellipse(0,0,this.r, this.r);
+			pop()
+
+  }
+    
+}
